794828969f7965b0e707f6ca5e4e0a268bc349e6
sik-flow <jherman1199@gmail.com>
2020-05-28 13:35:52 -0500

    update requirements

--- build/lib/ezinterpret/__init__.py
+++ build/lib/ezinterpret/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Jeff Herman"""
 __email__ = 'jherman1199@gmail.com'
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 
 from .ezinterpret import *
--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = ['pandas', 'matplotlib', 'numpy'. 'seaborn']
 
-setup_requirements = [ ]
+setup_requirements = ['pandas', 'matplotlib', 'numpy'. 'seaborn']
 
-test_requirements = [ ]
+test_requirements = ['pandas', 'matplotlib', 'numpy'. 'seaborn']
 
 setup(
     author="Jeff Herman",
